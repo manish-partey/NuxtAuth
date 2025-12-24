08265e840a20507dd35b9871f77f5d418734ccb3
Husein <huseinbhimani@yahoo.com>
2025-12-24 15:26:35 +0530

    Add MongoDB connection debug logging

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -4,6 +4,15 @@ const { connect } = mongoosePkg;
 
 export default defineNitroPlugin(async (nitroApp) => {
   const config = useRuntimeConfig();
+  
+  // Debug logging
+  console.log('=== MongoDB Connection Debug ===');
+  console.log('config.mongodbUri:', config.mongodbUri);
+  console.log('process.env.MONGO_CONNECTION_STRING:', process.env.MONGO_CONNECTION_STRING);
+  console.log('mongodbUri type:', typeof config.mongodbUri);
+  console.log('mongodbUri length:', config.mongodbUri?.length);
+  console.log('================================');
+  
   try {
     await connect(config.mongodbUri);
     console.log('MongoDB connected successfully!');
